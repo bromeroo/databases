5a2b4b34cd433ef9725ce3499a4837e612f392ba
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-07-11 23:20:41 +0200

    first week homework database

---
+++ Week1/databases/world-answers.sql
@@ -0,0 +1,12 @@
+use world;
+show tables;
+select Name from country where Population>8000000;
+select Name from country where Name LIKE "%land%";
+select Name from city where Population between 500000 and 1000000;
+select Name from country where Continent="Europe";
+select Name from country order by SurfaceArea desc;
+select Name from city where CountryCode="NLD";
+select Population from city where Name="Rotterdam";
+select Name from country order by SurfaceArea desc limit 10;
+select Name from country order by Population desc limit 10;
+select sum(Population) from country;
