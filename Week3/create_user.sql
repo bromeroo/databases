1ac7a0ab6a7cef3d77d451de13034afc16715767
bromeroo <bromeroo@users.noreply.github.com>
2019-07-29 14:47:23 +0200

    Missing by

--- Week3/create_user.sql
+++ Week3/create_user.sql
@@ -1,6 +1,6 @@
 
 First we create a user for our application, we will use this values in the connection in database.js.
-mysql> create user 'hyfclass4'@'localhost' identified with mysql_native_password y 'You have to select a good password here!';
+mysql> create user 'hyfclass4'@'localhost' identified with mysql_native_password by 'You have to select a good password here!';
 Query OK, 0 rows affected (0.10 sec)
 
 We grant everything because IT people like me is lazy but you should try giving the user only privileges on select, insert, update and delete and see what happens
